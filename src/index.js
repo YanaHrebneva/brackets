5aff845159275761d03cb6278b8b7f70c07761d2
YanaHrebneva <72386684+YanaHrebneva@users.noreply.github.com>
2021-02-22 22:22:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrayOfBrasckets = bracketsConfig.map( e => e.join(''));
+
+    for (let i = 0; i < arrayOfBrasckets.length; i++){
+      if (str.includes(arrayOfBrasckets[i])) {
+        str = str.replace(arrayOfBrasckets[i], '');
+        i = -1;
+      }
+    } 
+
+    return str.length != 0;    
 }
